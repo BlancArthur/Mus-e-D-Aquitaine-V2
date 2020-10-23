c5e1421c2f9b384a1b9322fdfd39964bbc792873
DrAlvina24 <72981601+DrAlvina24@users.noreply.github.com>
2020-10-23 14:09:58 +0200

    Add files via upload

--- js/btq.js
+++ js/btq.js
@@ -2,6 +2,11 @@ function buttonToggle(where, pval, nval) {
     var elem = document.getElementById(where.attributes.rel.value);
     where.value = (where.value == pval) ? nval : pval;
     elem.style.display = (elem.style.display == 'block') ? 'none' : 'block';
+
+    if(elem.style.display == 'block')
+    {
+      window.scrollTo(elem.scrollLeft, elem.scrollTop + elem.height);
+    }
 }
 
 $(document).on("click mousemove",function(e){ 
@@ -13,9 +18,39 @@ $(document).on("click mousemove",function(e){
     $(".wheel").css("transform","translate3d("+newposX+"px,"+newposY+"px,0px)");
 });
 
-var elements = document.getElementsByClassName("btq-gi-column");
-var i;
-for (i = 0; i < elements.length; i++) 
+
+function selectImg(btn)
 {
-  elements[i].style.flex = "25%";
+
+  for(let i = 1; i <= 5; i++) 
+  {
+    $("#img-" + i).css("display", "none");
+  }
+
+  $(".btq-pp-img").attr('src', 'img/btq/pp/'+ btn.value +'.jpg');
+  $("#img-" + btn.value).css("display", "block");
+
+  console.log(btn.value);
+  console.log("#img-" + btn.value);
 }
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
